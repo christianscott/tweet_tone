dbd52928d8e69790db9c48b70031189fd66094a6
Christian <christianfscott@gmail.com>
2016-09-15 02:06:14 +1200

    removed stuff

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,4 @@
 /// <reference path="../typings/index.d.ts"/>
-function sendPokeRequest() {
-    var url = "http://pokeapi.co/api/v2/" + query.category + "/";
-    $.ajax({
-        url: url,
-        type: "GET"
-    })
-        .done(function (data) {
-        console.log(data);
-    })
-        .fail(function (error) {
-        console.log(error);
-    });
-}
-;
-/// <reference path="../typings/index.d.ts"/>
 var result = $(".result")[0];
 function sendTweetToneRequest(username) {
     var url = "https://immense-sea-71091.herokuapp.com/tweetTone/" + username;
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,5 @@
 /// <reference path="../typings/index.d.ts"/>
 
-function sendPokeRequest(): void {
-  let url = `http://pokeapi.co/api/v2/${query.category}/`
-  $.ajax({
-    url: url,
-    type: "GET"
-  })
-    .done(function (data) {
-      console.log(data);
-    })
-    .fail(function (error) {
-      console.log(error);
-    });
-};
-
-/// <reference path="../typings/index.d.ts"/>
-
 let result = $(".result")[0];
 
 function sendTweetToneRequest(username: string): void {
